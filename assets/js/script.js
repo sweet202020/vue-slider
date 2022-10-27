bb56ebe97d3da408bd229a4769cf29986dd8a7bc
sweet202020 <dolcealessio1@gmail.com>
2022-10-27 17:18:41 +0200

    removed class active in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ createApp({
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 },
             ],
-            classActive: 'active'
+            
         }
     },
     methods: {
